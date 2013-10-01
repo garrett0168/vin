44b3cd9ee420c4a076e39c8173ed5e24d99f7725
Richard Young <richard.j.young@gmail.com>
2013-09-30 19:50:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vin::Application.config.secret_key_base = '322d22f3adb7b2c524cbbca7f81272f89831c291770511bf0a2c0a934cb0f729fac3035b07f111184986a28326b24c5308153a2ad99ac0dc5e629495324d77be'
