efd56cfca78689d0b673a891ccfc7a0f6b90060a
Richard Young <richard.j.young@gmail.com>
2013-10-04 07:33:31 -0500

    moved images to top. updated styles a little

--- app/assets/javascripts/ui-bootstrap.js
+++ app/assets/javascripts/ui-bootstrap.js
@@ -3332,7 +3332,7 @@ angular.module("template/alert/alert.html", []).run(["$templateCache", function(
 
 angular.module("template/carousel/carousel.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/carousel/carousel.html",
-    "<div ng-mouseenter=\"pause()\" ng-mouseleave=\"play()\" class=\"carousel\">\n" +
+    "<div ng-mouseenter=\"pause()\" ng-mouseleave=\"play()\" class=\"carousel slide\">\n" +
     "  <ol class=\"carousel-indicators\" ng-show=\"slides().length > 1\">\n" +
     "    <li ng-repeat=\"slide in slides()\" ng-class=\"{active: isActive(slide)}\" ng-click=\"select(slide)\"></li>\n" +
     "  </ol>\n" +
